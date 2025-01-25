7de8b4ef1e3d6a0ce6f67abfbc6ab54d4b0dc054
Shorya Sharma <shoryasharma9560893769@gmail.com>
2025-01-25 20:01:40 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,19 @@
+print(type("hello there 30!"))
+user_name='supercoder'
+password='topsecret'
+long_string='''
+WOW
+_ _
+O O
+'''
+print(long_string)
+first_name='shorya'
+last_name='sharma'
+full_name=first_name + ' ' + last_name
+print(full_name)
+
+
+a=str(100)
+b=int(a)
+c=type(b)
+print(c)

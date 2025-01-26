503e0c79bbb888839ba19560bcc28f95a3c140f2
Shorya Sharma <shoryasharma9560893769@gmail.com>
2025-01-26 19:52:29 +0530

    Add files via upload

---
+++ builtin functions.py
@@ -0,0 +1,12 @@
+len('hello')
+print(len('hello'))
+greet='hello'
+print(greet[0:5])
+
+
+quote='to be or not to be'
+print(quote.upper())
+quote='to be or not to be'
+print(quote.find('or'))
+quote='to be or not to be'
+print(quote.replace('be','me'))

dea175084929214a40b1b66f010ad4c4cd3ce9df
Shorya Sharma <shoryasharma9560893769@gmail.com>
2025-01-27 21:33:46 +0530

    Add files via upload

---
+++ list methods.py
@@ -0,0 +1,36 @@
+
+# basket=[1,2,3,4,5]
+
+# #adding
+# basket.append(100)
+# new_list=basket
+
+# print(new_list) 
+# basket.insert(4,100)
+# new_list=basket
+
+# print(new_list)
+
+
+# #removing
+# basket.pop()
+# basket.pop(0)
+
+# print(basket)
+# new_list=basket.clear()
+# print(new_list)
+# sentence='!'
+# new_sentence=sentence.join(['hi','my','name','is','shorya'])
+# print(new_sentence)
+
+
+#list unpacking
+a,b,c,*other,d=[1,2,3,4,5,6,7]
+
+print(a)
+print(b)
+print(c)
+print(other)
+print(d)
+
+

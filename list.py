4596c68c13ebd4758b6cccb48fb5e6106c63da4f
Shorya Sharma <shoryasharma9560893769@gmail.com>
2025-01-27 21:33:24 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+li=[1,2,3,4,5]
+li2=['a','b','c']
+li3=['car','bike','cycle']
+print( li,li2,li3)
+
+amazon_cart=['shoes','sunglasses','bag']
+print(amazon_cart[0])
+
+zepto_cart=['toys','games','grapes','chips']
+print(zepto_cart[0:2])
+print(zepto_cart[0::2])
+print(zepto_cart[0::3])
+zepto_cart[0]='juice'
+print(zepto_cart)
+new_cart=amazon_cart
+new_cart[0]='gum'
+print(new_cart)

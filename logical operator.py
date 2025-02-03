2a297982c16218745f7cb387b988cf62d682434b
Shorya Sharma <shoryasharma9560893769@gmail.com>
2025-02-03 13:44:22 +0530

    Add files via upload

---
+++ logical operator.py
@@ -0,0 +1,21 @@
+#logical operator
+# print(4>5)
+# print(4<5)
+# print('a'>'b')
+# print(5==7)
+# print(9==9)
+# print(1<2<3<4<5)
+# print(0!=3)
+# print(1<=4)
+# print(not True)
+# print(not(1==1))
+
+
+is_magician=False
+is_expert=True
+if is_magician and is_expert:
+    print("you are master magician")
+elif is_magician and not is_expert:
+    print("atleast you're getting there ") 
+elif not is_magician:
+    print("you need magic powers")

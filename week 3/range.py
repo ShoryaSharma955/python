10232ad924ceab10529ac95dd8625b4da8ec559c
Shorya Sharma <shoryasharma9560893769@gmail.com>
2025-03-06 20:40:20 +0530

    Add files via upload

---
+++ week 3/range.py
@@ -0,0 +1,12 @@
+# for _ in range(0,10):
+    # print(_)
+# for _ in range(0,10,2):
+#      print(_)
+
+# for _ in range(10,0,-1):
+#     print(_)
+
+for _ in range(3):
+    print(list(range(10)))
+
+

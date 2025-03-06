d22e739e29264820629fe179587c86a715f14ac2
Shorya Sharma <shoryasharma9560893769@gmail.com>
2025-03-06 20:41:02 +0530

    Add files via upload

---
+++ week 3/while loops.py
@@ -0,0 +1,29 @@
+# i=50
+# while i<50:
+#     print(i)
+#     i +=1
+#     break
+# else:
+#     print("done with all the work ")
+
+#     while True :
+#         response=input('say something:')
+#         if(response=='bye'):
+#             break
+
+
+
+
+
+my_list=[1,2,3]
+for item in my_list:
+    print(item)
+    continue
+
+i=0
+while i<len(my_list):
+    print(my_list[i])
+    i+=1
+    continue
+
+    
